8f03828cbd6246b9143b9d16165930411b241fa7
shanerbn <shane@converted.co.uk>
2015-01-22 21:36:26 +0000

    made test page with task panel

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
-  get 'about' =>"pages#about"
+  get 'about' => "pages#about"
+  get 'test' => "pages#test"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
